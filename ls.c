b3e11bcf9b68ad182af8972af4ed55cfad664da4
ararune <jcjurkic@gmail.com>
2023-04-27 20:57:40 +0200

    ls blue color for directories

--- ls.c
+++ ls.c
@@ -5,8 +5,9 @@
 #include <fnmatch.h>
 #include "ls.h"
 
-// This function is used to compare two `dirent` structures by name
-// It is passed as a parameter to the `qsort` function, which will use it to sort an array of `dirent` pointers
+#define COLOR_DIR "\033[38;5;39m"  // Blue
+#define COLOR_FILE "\033[0m"  // Reset color
+
 int compare_names(const void *a, const void *b) {
   const struct dirent **entry1 = (const struct dirent **)a;
   const struct dirent **entry2 = (const struct dirent **)b;
@@ -35,26 +36,27 @@ int lsh_ls(char **args) {
       pattern = args[1];
     }
   }
-  // Call `scandir` to get an array of `dirent` structures for the current       directory
-  // The `alphasort` function is used to sort the entries alphabetically
+
   num_entries = scandir(".", &entries, NULL, alphasort);
   if (num_entries == -1) {
     perror("lsh");
     return 1;
   }
 
-  // Loop through each entry in the array and print the name if it matches the specified pattern and is not hidden
   for (int i = 0; i < num_entries; i++) {
     if (!show_hidden && entries[i]->d_name[0] == '.') {
       continue; // skip over hidden files and directories
     }
     if (fnmatch(pattern, entries[i]->d_name, FNM_PATHNAME) == 0) {
-      printf("%s  ", entries[i]->d_name);
+      if (entries[i]->d_type == DT_DIR) {
+        printf(COLOR_DIR "%s  " COLOR_FILE, entries[i]->d_name);
+      } else {
+        printf("%s  ", entries[i]->d_name);
+      }
     }
   }
   printf("\n");
 
-  // Free the memory allocated for the `dirent` structures and the array
   for (int i = 0; i < num_entries; i++) {
     free(entries[i]);
   }
