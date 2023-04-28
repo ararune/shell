b40827f8ef15533677db86abbde495e73bd865a8
ararune <jcjurkic@gmail.com>
2023-04-28 18:46:54 +0200

    cd ~ to HOME

--- cd.c
+++ cd.c
@@ -2,14 +2,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "cd.h"
+
 int lsh_cd(char **args)
 {
   if (args[1] == NULL) {
     fprintf(stderr, "lsh: expected argument to \"cd\"\n");
   } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
+    if (strcmp(args[1], "~") == 0) {
+      char* home_dir = getenv("HOME");
+      if (home_dir == NULL) {
+        perror("lsh");
+        return 1;
+      } else {
+        if (chdir(home_dir) != 0) {
+          perror("lsh");
+          return 1;
+        }
+      }
+    } else {
+      if (chdir(args[1]) != 0) {
+        perror("lsh");
+        return 1;
+      }
     }
   }
   return 1;
